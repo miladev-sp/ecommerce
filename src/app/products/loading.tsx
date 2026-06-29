42a1b79f7d29add66b531ac3c20bb6ce455765e0
miladev-sp <miladevzeynali@gmail.com>
2026-06-29 19:18:23 +0330

    Create loading.tsx

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="h-10 w-64 bg-gray-100 animate-pulse rounded-md" />
+      <div className="grid grid-cols-2 gap-y-7 gap-x-4 min-[1211px]:grid-cols-3">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <div key={i} className="w-full shrink-0 animate-pulse">
+            <div className="bg-gray-200 rounded-xl aspect-square mb-3" />
+            <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
+            <div className="h-4 bg-gray-200 rounded w-1/2" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
