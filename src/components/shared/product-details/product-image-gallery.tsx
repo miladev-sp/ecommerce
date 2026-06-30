bb8ae3fb0299805b3ba202e3ed8adb835c76349c
miladev-sp <miladevzeynali@gmail.com>
2026-07-01 01:37:03 +0330

    Update product-image-gallery.tsx

--- src/components/shared/product-details/product-image-gallery.tsx
+++ src/components/shared/product-details/product-image-gallery.tsx
@@ -16,22 +16,25 @@ export default function ImageGallery({ product }: Prop) {
     <div className="flex flex-col items-center justify-center  sm:justify-around  md:flex-row-reverse  xl:h-full xl:w-full xl:gap-4 md:gap-6 ">
       <div className="bg-[#F0EEED] rounded-[20px] w-64.5 h-72.5 flex items-center justify-center xl:w-112.5  xl:h-full xl:flex-2 relative overflow-hidden ">
         {!loadedMain.has(selectedImage) && (
-          <div className="absolute inset-0 bg-[#E0DDDA] animate-pulse rounded-[20px]" />
+          <div className="absolute inset-0 bg-[#E0DDDA] animate-pulse rounded-[20px] z-0" />
         )}
-        <Image
-          src={selectedImage}
-          alt={product.title}
-          className={`object-contain xl:w-full xl:h-auto transition-opacity duration-200 ${
-            loadedMain.has(selectedImage) ? "opacity-100" : "opacity-0"
-          }`}
-          fill
-          priority
-          onLoad={() =>
-            setLoadedMain((prev) => new Set(prev).add(selectedImage))
-          }
-          sizes="(max-width: 640px) 200px, (max-width: 1280px) 300px, 450px"
-          fetchPriority="high"
-        />
+        {product.images.map((image, index) => (
+          <Image
+            key={image}
+            src={image}
+            alt={product.title}
+            className={`object-contain xl:w-full xl:h-auto transition-opacity duration-200 ${
+              selectedImage === image && loadedMain.has(image)
+                ? "opacity-100 z-10"
+                : "opacity-0 z-0"
+            }`}
+            fill
+            priority={index === 0}
+            onLoad={() => setLoadedMain((prev) => new Set(prev).add(image))}
+            sizes="(max-width: 640px) 200px, (max-width: 1280px) 300px, 450px"
+            fetchPriority={index === 0 ? "high" : "auto"}
+          />
+        ))}
       </div>
 
       <div className="flex gap-3 overflow-x-auto hide-scrollbar max-sm:w-full   mt-4 sm:justify-between md:flex-col   xl:flex-1 ">
@@ -54,7 +57,7 @@ export default function ImageGallery({ product }: Prop) {
                 className={`object-contain transition-opacity duration-200 ${
                   loadedThumbs.has(image) ? "opacity-100" : "opacity-0"
                 }`}
-                sizes="(max-width: 640px) 200px, (max-width: 1280px) 300px, 450px"
+                sizes="(max-width: 640px) 112px, 150px"
               />
             </div>
           </div>
